72bd944a770040aef449c42cd1c4f1789081e80a
qian.zhang2 <qian.zhang2@thoughtworks.com>
2024-04-02 17:17:21 +0800

    update custom plugin and add echarts

--- src/lib/my-custom-plugin/index.ts
+++ src/lib/my-custom-plugin/index.ts
@@ -14,19 +14,21 @@ export const MyCustomPlugin = (
   const execute = async (inputs: PluginParams[]): Promise<PluginParams[]> => {
     function generateFile(filePath: string) {
       // Create file in user dir
+      console.log(filePath);
     }
 
     function fillFileContent(input: PluginParams) {
       // Get we needed data from input
       // Start fill file content
+      console.log(input);
     }
 
-    const filePath = ''
-    generateFile(filePath)
+    const filePath = '';
+    generateFile(filePath);
     return inputs.map(input => {
       // Validate input, it is have our needed parameter? (energy, timestamp...)
       // Get generate file path, need considerate this is come from user or us?
-      fillFileContent(input)
+      fillFileContent(input);
       globalConfig;
 
       return input;
